a4ed91640c6101b81fe5bbfcbde9938a503d237c
lucas-prat-dev <pratlucas3@gmail.com>
2026-06-21 19:34:47 -0300

    boceto a mejorar de tarjeta de invitación

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const weddingDate = new Date("2026-11-27T18:00:00");
+
+function updateCountdown() {
+  const now = new Date();
+
+  const difference = weddingDate - now;
+
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+
+  document.getElementById("hours").innerText = hours;
+
+  document.getElementById("minutes").innerText = minutes;
+
+  document.getElementById("seconds").innerText = seconds;
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, 1000);
+
+function openDressCode() {
+  document.getElementById("dressCodeModal").style.display = "block";
+}
+
+function closeDressCode() {
+  document.getElementById("dressCodeModal").style.display = "none";
+}
+
+window.onclick = function (event) {
+  const modal = this.document.getElementById("dressCodeModal");
+
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
